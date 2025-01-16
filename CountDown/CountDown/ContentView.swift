0adff5062bce1a159fc81ac3b66e0697bb3f47df
SirarpiBayramyan <you@example.com>
2025-01-16 15:51:47 +0400

    CountDown initial

---
+++ CountDown/CountDown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountDown
+//
+//  Created by Sirarpi Bayramyan on 16.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountDown/CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Sirarpi Bayramyan on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
