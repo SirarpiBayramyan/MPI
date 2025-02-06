422cff5a75109690f4209e0745a53743d8b5e227
SirarpiBayramyan <you@example.com>
2025-02-06 15:45:41 +0400

    Fetch data by using combine features

---
+++ Fetchdate/Fetchdate/FetchdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchdateApp.swift
+//  Fetchdate
+//
+//  Created by Sirarpi Bayramyan on 06.02.25.
+//
+
+import SwiftUI
+
+@main
+struct FetchdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
